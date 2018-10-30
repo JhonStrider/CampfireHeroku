0dc46ba32d0019c175b858bd8163e18f56bc0bb0
JhonStrider <jmpinheiro11@gmail.com>
2018-10-30 16:27:48 -0300

    Update DemoCampfireApplication.java

--- src/main/java/com/herokuSas/demo_Campfire/DemoCampfireApplication.java
+++ src/main/java/com/herokuSas/demo_Campfire/DemoCampfireApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoCampfireApplication {
 
 	public static void main(String[] args) {
+		System.out.println("teste1");
+
 		SpringApplication.run(DemoCampfireApplication.class, args);
 	}
 }
