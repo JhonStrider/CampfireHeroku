136a9c95ccea84263fff093eb42640974fd035c2
JhonStrider <jmpinheiro11@gmail.com>
2018-10-30 16:21:17 -0300

    tesste
    teste

--- src/main/java/com/herokuSas/demo_Campfire/DemoCampfireApplication.java
+++ src/main/java/com/herokuSas/demo_Campfire/DemoCampfireApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoCampfireApplication {
 
 	public static void main(String[] args) {
+		system.out.println('hello word');
 		SpringApplication.run(DemoCampfireApplication.class, args);
 	}
 }
