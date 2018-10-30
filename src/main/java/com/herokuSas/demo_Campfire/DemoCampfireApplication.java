24594545a0e4852aa08572ba81e860fbe3369928
SULAMERICA\jmp0218 <joaomatheus.noorden@sulamerica.com.br>
2018-10-30 13:57:34 -0300

    Vlw Denys obrigado pela coperação e apoio e é nos

---
+++ src/main/java/com/herokuSas/demo_Campfire/DemoCampfireApplication.java
@@ -0,0 +1,12 @@
+package com.herokuSas.demo_Campfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCampfireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCampfireApplication.class, args);
+	}
+}
---
+++ src/test/java/com/herokuSas/demo_Campfire/DemoCampfireApplicationTests.java
@@ -0,0 +1,16 @@
+package com.herokuSas.demo_Campfire;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoCampfireApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
